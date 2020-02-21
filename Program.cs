ddd009ae8f180ca57134f3de419e1f4b2e5ab5d5
Zalka.Péter <Zalka.Pter@C41-15>
2020-02-21 09:02:58 +0100

    Adatszerkezet elkészült.

--- Program.cs
+++ Program.cs
@@ -10,8 +10,8 @@ namespace Órai0221
     {
         static void Main(string[] args)
         {
-            string[] Összjármű = { "ABC-123", "QWE-456", "JKL-678", "BNM-789", "FVH-618", "LOK-783", "LPO-445", "JRT-767" };
-            List<string> Dinamikus = new List<string>();
+            string[] teljes = { "ABC-123", "QWE-456", "JKL-678", "BNM-789", "ASD-125", "LOK-783", "LPO-445", "JRT-767" };
+            List<string> bent = new List<string>();
 
 
 
