bfdfbf3347cbbbb28fd8956222d4c4421352b238
JedlikZP <60391140+JedlikZP@users.noreply.github.com>
2020-02-21 08:32:30 +0100

    Próba

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Órai0221
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Összjármű = { "ABC-123", "QWE-456", "JKL-678", "BNM-789", "FVH-618", "LOK-783", "LPO-445", "JRT-767" };
+            List<string> Dinamikus = new List<string>();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
