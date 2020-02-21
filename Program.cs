5ed7835babec0351415f6f2434159ff4540b01b2
Zalka.Péter <Zalka.Pter@C41-15>
2020-02-21 09:11:08 +0100

    Menürendszer.

--- Program.cs
+++ Program.cs
@@ -12,8 +12,23 @@ namespace Órai0221
         {
             string[] teljes = { "ABC-123", "QWE-456", "JKL-678", "BNM-789", "ASD-125", "LOK-783", "LPO-445", "JRT-767" };
             List<string> bent = new List<string>();
+            string választás;
+            do
+            {
+                Console.WriteLine("Elmegy (E)");
+                Console.WriteLine("Bejön (B)");
+                Console.WriteLine("Kilépés (K)");
+                választás = Console.ReadLine();
 
+                switch (választás)
+                {
+                    case "E":Elmegy(teljes);break;
+                    case "B":Bejön(teljes);break;
+                    case "K":Kilépés();break;
+                    default: Console.WriteLine("Nem megfelelő választás!");break;
+                }
 
+            } while (választás!="K");
 
             Console.ReadKey();
         }
